4d4e451f666d562f9afb890a52bf12d5f1d96da4
Bizarria Cutri <labt@gft.com>
2023-06-15 16:20:19 -0300

    acionando a pipeline

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -50,8 +50,6 @@ Cypress.Commands.add('fillSignupFormAndSubmit', (email, password) => {
 })
 
 Cypress.Commands.add('login', (
-  //username = Cypress.env('USER_EMAIL'),
-  //password = Cypress.env('USER_PASSWORD'),
   { cacheSession = true } = {}
 ) => {
   const login = () => {
